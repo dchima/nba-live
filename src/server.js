a4c797f0fa82cd4b03b9fe25f5f41b3502703621
dchima <dcnnad@gmail.com>
2019-09-17 11:21:36 +0100

    edited sever

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import express, { static as staticfunction } from 'express';
-import { resolve } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(staticfunction(resolve(__dirname, '../../build')));
+app.use(express.static(path.resolve(__dirname, '../../build')));
 app.use('*', (req, res) => {
-  res.sendFile(resolve(__dirname, '../../build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
